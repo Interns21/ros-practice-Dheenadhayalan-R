f9d5c9ae5cf28e9b38e80ac62ff5ee5959f5e7be
Dheenadhayalan R <dheenadhayalanr19@gmail.com>
2021-06-11 15:24:57 +0530

    Add Comments to teleop scripts

--- refr_learns/src/chap8/scripts/key_publisher.py
+++ refr_learns/src/chap8/scripts/key_publisher.py
@@ -5,7 +5,7 @@ import rospy
 from std_msgs.msg import String
 
 if __name__ == '__main__':
-    key_pub = rospy.Publisher('keys', String, queue_size=1)
+    key_pub = rospy.Publisher('keys', String, queue_size=1)  # publish in 'key' topic
     rospy.init_node('keyboard_driver')
     rate = rospy.Rate(100)
     old_attr = termios.tcgetattr(sys.stdin)
@@ -13,6 +13,6 @@ if __name__ == '__main__':
     print 'Publishing key strokes press Ctrl-C to exit...'
     while not rospy.is_shutdown():
         if select.select([sys.stdin], [], [], 0)[0] == [sys.stdin]:
-            key_pub.publish(sys.stdin.read(1))
+            key_pub.publish(sys.stdin.read(1))  # publish key strokes
         rate.sleep()
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_attr)
--- refr_learns/src/chap8/scripts/key_to_twist.py
+++ refr_learns/src/chap8/scripts/key_to_twist.py
@@ -7,13 +7,17 @@ from geometry_msgs.msg import Twist
 
 class TwistConverter:
     def __init__(self):
+        """
+        Initialize publishers, subscribers, common variables and parameters
+        publish Twist message of a key press in 'cmd_vel' topic
+        """
         self.target_vel = self.current_vel = Twist()
         self.vel_scales = [0.1, 0.1]
         self.vel_accels = [1.0, 1.0]
-        self.key_mapping = {'w': [1, 0], 'x': [-1, 0], 'a': [0, 1], 'd': [0, -1], 's': [0, 0]}
-        self.vel_scales[0] = self.fetch_params('~linear_scale', self.vel_scales[0])
+        self.key_mapping = {'w': [1, 0], 'x': [-1, 0], 'a': [0, 1], 'd': [0, -1], 's': [0, 0]}  # linear and angular velocities' direction for respective key press
+        self.vel_scales[0] = self.fetch_params('~linear_scale', self.vel_scales[0])  # obtaining scales from commandline or default value
         self.vel_scales[1] = self.fetch_params('~angular_scale', self.vel_scales[1])
-        self.vel_accels[0] = self.fetch_params('~linear_accel', self.vel_accels[0])
+        self.vel_accels[0] = self.fetch_params('~linear_accel', self.vel_accels[0])  # obtaining accleration from commandline or default value
         self.vel_accels[1] = self.fetch_params('~angular_accel', self.vel_accels[1])
 
         self.key_sub = rospy.Subscriber('keys', String, self.key_callback)
@@ -24,6 +28,9 @@ class TwistConverter:
         self.execute()
 
     def execute(self):
+        """
+        Execution of teleop
+        """
         print('Publishing Twist messages press Ctrl-C to exit...')
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
@@ -31,22 +38,31 @@ class TwistConverter:
             rate.sleep()
 
     def fetch_params(self,name, default):
-        if rospy.has_param(name):
+        """
+        Returns parameters from command line if provided or returns default values
+        """
+        if rospy.has_param(name):  # return given parameter else default parameter
             return rospy.get_param(name)
         else:
             rospy.logwarn('%s scale not provided, using default value %.1f'%\
-            (name, default))
+            (name, default))  # Warn if parameter not provided
             return default
 
     def key_callback(self, key):
-        if key.data[0] in self.key_mapping:
-            vels = self.key_mapping[key.data[0]]
+        """
+        Callback for key press messages and sets target Twist
+        """
+        if key.data[0] in self.key_mapping:  # if the pressed key is in w, a, s, d, x
+            vels = self.key_mapping[key.data[0]]  # obtain respective velocity direction
             cmd = Twist()
-            cmd.linear.x = vels[0] * self.vel_scales[0]
+            cmd.linear.x = vels[0] * self.vel_scales[0]  # Scale the velocities
             cmd.angular.z = vels[1] * self.vel_scales[1]
-            self.target_vel = cmd
+            self.target_vel = cmd  # set target Twist
 
     def reach_vel(self, target_vel):
+        """
+        Publishes the next Twist and get ready for next loop
+        """
         current_time = rospy.Time.now()
 
         next_vel = Twist()
@@ -55,26 +71,31 @@ class TwistConverter:
         target_vel.linear.x,
         current_time,
         self.last_pub_time,
-        self.vel_accels[0])
+        self.vel_accels[0])  # set next linear velocity to publish
 
         next_vel.angular.z = self.next_vel(self.current_vel.angular.z,
         target_vel.angular.z,
         current_time,
         self.last_pub_time,
-        self.vel_accels[1])
+        self.vel_accels[1])  # set next angular velocity to publish
 
-        self.cmd_vel_pub.publish(next_vel)
+        self.cmd_vel_pub.publish(next_vel)  # publish next_vel and get ready for next loop
         self.last_pub_time = current_time
         self.current_vel = next_vel
 
     def next_vel(self, current_vel, target_vel, current_time, last_time, accel):
-        if current_vel == target_vel:
+        """
+        Checks if the target is reached and gives the next velocity accordingly
+        considering current velocity, target velocity, acclerations and
+        time elapsed
+        """
+        if current_vel == target_vel:  # if target velocity reached return same
             return target_vel
-        delta_sign = 1.0 if target_vel > current_vel else -1.0
-        delta_vel = accel * (current_time - last_time).to_sec()
-        vel_next = current_vel + delta_sign * delta_vel
+        delta_sign = 1.0 if target_vel > current_vel else -1.0  # check whether to acclerate or decelerate
+        delta_vel = accel * (current_time - last_time).to_sec()  # obtain the magnitude of velocity change using acceleration and time passed
+        vel_next = current_vel + delta_sign * delta_vel  # the next expected velocity
 
-        if delta_sign * vel_next >= delta_sign * target_vel :
+        if delta_sign * vel_next >= delta_sign * target_vel :  # check if reached target
             return target_vel
         else:
             return vel_next
