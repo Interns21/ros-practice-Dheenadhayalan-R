fab2701a591270230ae32cde9ef4c7ef1b23f62a
Dheenadhayalan R <dheenadhayalanr19@gmail.com>
2021-06-16 16:12:54 +0530

    Code robot model and state tutorial

---
+++ moveit_ws/src/my_codes/scripts/robot_model_state.cpp
@@ -0,0 +1,63 @@
+#include <ros/ros.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/robot_model/robot_model.h>
+#include <moveit/robot_state/robot_state.h>
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "robot_model_and_robot_state_tutorial");
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+  robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
+  ROS_INFO("Model frame: %s", kinematic_model->getModelFrame().c_str());
+
+  robot_state::RobotStatePtr kinematic_state(new robot_state::RobotState(kinematic_model));
+  kinematic_state->setToDefaultValues();
+  const robot_state::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("panda_arm");
+
+  const std::vector<std::string>& joint_names = joint_model_group->getVariableNames();
+
+  std::vector<double> joint_values;
+  kinematic_state->copyJointGroupPosition(joint_model_group, joint_values);
+  for (std::size_t i = 0; i < joint_names.size(); ++i){
+    ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
+  }
+
+  joint_values[0] = 5.57;
+  kinematic_state->setJointGroupPosition(joint_model_group, joint_values);
+
+  ROS_INFO_STREAM("Current state is " << (kinematic_state->satsfiesBounds() ? "valid" : "not valid"));
+
+  kinematic_state->enforceBounds();
+  ROS_INFO_STREAM("Current state is " << (kinematic_state->satsfiesBounds() ? "valid" : "not valid"));
+
+  kinematic_state->setToRandomPosition(joint_model_group);
+  const Eigen::Isometry3d& end_effector_state = kinematic_state->getGlobalLinkTransform("panda_link8");
+
+  ROS_INFO_STREAM("Translation: \n" << end_effector_state.translstion() << "\n");
+  ROS_INFO_STREAM("Rotation \n" << end_effector_state.rotation() << "\n");
+
+  double timeout = 0.1;
+  bool found_ik = kinematic_state->setFormIK(joint_model_group, end_effector_state, timeout);
+
+  if (found_ik){
+    kinematic_state->copyJointGroupPosition(joint_model_group, joint_values);
+    for (std::size_t i = 0; i < joint_names.size(); ++i){
+      ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
+    }
+  }
+  else{
+    ROS_INFO("Dind not find IK solution");
+  }
+
+  Eigen::vector3d refrence_point_position(0.0, 0.0, 0.0);
+  Eigen::MatrixXd jacobian;
+  kinematic_state->getJacobian(joint_model_group,
+  kinematic_state->getLinkModel(joint_model_group->getLinkModelNames().back()),
+  refrence_point_position, jacobian);
+  ROS_INFO_STREAM("Jacobian: \n" << jacobian << "\n");
+
+  ros::shutdown();
+  return 0;
+}
