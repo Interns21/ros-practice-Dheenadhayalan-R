7470a1a7d378d48e8987d390cef4ab80dd522109
Dheenadhayalan R <dheenadhayalanr19@gmail.com>
2021-07-20 14:36:50 +0530

    Tweak values and debug but not yet resolved

--- robot_modeling_ws/src/moveit_revisit_pkg/src/demo_1.cpp
+++ robot_modeling_ws/src/moveit_revisit_pkg/src/demo_1.cpp
@@ -2,10 +2,10 @@
 #include <tf2/LinearMath/Quaternion.h>
 
 
-float get_pose_val(int i, char xz){
-  float pose_x[] = {0.1, 0.1, -0.1, -0.1};
-  float pose_z[] = {0.05, 0.25, 0.25, 0.05};
-  return (xz == 'x')?pose_x[i]:pose_z[i];
+float get_pose_val(int i, char xy){
+  float pose_x[] = {0.05, 0.05, -0.05, -0.05};
+  float pose_y[] = {-0.05, 0.05, 0.05, -0.05};
+  return (xy == 'x')?pose_x[i]:pose_y[i];
 }
 
 int main(int argc, char** argv){
@@ -21,16 +21,16 @@ int main(int argc, char** argv){
   move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
   tf2::Quaternion pitch_n90;
-  pitch_n90.setRPY(0, -1.57, 0);
+  pitch_n90.setRPY(-1.57, 0, 0);
 
   geometry_msgs::Pose pose;
   pose.orientation.x = pitch_n90[0];
   pose.orientation.y = pitch_n90[1];
   pose.orientation.z = pitch_n90[2];
   pose.orientation.w = pitch_n90[3];
-  pose.position.x = -0.1;
-  pose.position.y = 0.05;
-  pose.position.z = 0.05;
+  pose.position.x = -0.05;
+  pose.position.y = 0.1;
+  pose.position.z = 0.25;
 
   move_group.setPoseTarget(pose);
   moveit::planning_interface::MoveGroupInterface::Plan plan;
@@ -43,11 +43,13 @@ int main(int argc, char** argv){
 
   move_group.execute(plan);
 
-  std::vector<moveit::planning_interface::MoveGroupInterface::Plan> plan_to_follow;
+  std::vector<geometry_msgs::Pose> poses_to_follow;
 
   for(int i=0; i<4; i++){
     pose.position.x = get_pose_val(i, 'x');
-    pose.position.y = get_pose_val(i, 'z');
+    pose.position.z = 0.25 + get_pose_val(i, 'y');
+    poses_to_follow.push_back(pose);
+    /*
     move_group.setPoseTarget(pose);
     success = (move_group.plan(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
     if(success){
@@ -59,14 +61,16 @@ int main(int argc, char** argv){
     }
     robot_state::RobotState start_state(*move_group.getCurrentState());
     start_state.setFromIK(joint_model_group, pose);
-    move_group.setStartState(start_state);
+    move_group.setStartState(start_state);*/
   }
 
+  moveit_msgs::RobotTrajectory trajectory;
+  const double jump_treshold = 0.05;
+  const double eef_step = 0.01;
+  double fraction = move_group.computeCartesianPath(poses_to_follow, eef_step, jump_treshold, trajectory);
+
   while(ros::ok()){
-    std::vector<moveit::planning_interface::MoveGroupInterface::Plan>::iterator plan_4_now;
-    for(plan_4_now = plan_to_follow.begin(); plan_4_now < plan_to_follow.end(); plan_4_now++){
-      move_group.execute(*plan_4_now);
-    }
+    move_group.execute(trajectory);
   }
 
   return 0;
