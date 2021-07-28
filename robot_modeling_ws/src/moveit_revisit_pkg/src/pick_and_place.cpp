0949e0e631664e3b69cc2d2fb70e4fc95e194c63
Dheenadhayalan R <dheenadhayalanr19@gmail.com>
2021-07-28 23:02:51 +0530

    Make command line parameters

--- robot_modeling_ws/src/moveit_revisit_pkg/src/pick_and_place.cpp
+++ robot_modeling_ws/src/moveit_revisit_pkg/src/pick_and_place.cpp
@@ -5,23 +5,63 @@
 
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
-const double OBJECT_Z = 0.2;
-const double ALL_DIST = 0.3;
-const double EEF_OFFSET = 0.145;
-const double EXTRA_OFFSET = 0.01;
-const double OBJECT_LENGTH = 0.06;
-const double OBJECT_WIDTH = 0.03;
 
-const double TABLE_HEIGHT = OBJECT_Z - OBJECT_LENGTH / 2;
+double TABLE_HEIGHT = 0.2;
+double TABLE_DIST = 0.4;
+double EEF_OFFSET = 0.145;
+double EXTRA_OFFSET = 0.01;
+double OBJECT_HEIGHT = 0.06;
+double OBJECT_WIDTH = 0.03;
 
+void setParameters(ros::NodeHandle& node_handle){
+    if(node_handle.getParam("table_dist", TABLE_DIST)){
+        ROS_INFO("Table spawning distance from origin is set to %lf", TABLE_DIST);
+    }
+    else{
+        ROS_INFO("Default: Table spawning distance from origin is set to %lf", TABLE_DIST);
+    }
+    
+    if(node_handle.getParam("table_height", TABLE_HEIGHT)){
+        ROS_INFO("Height of the table is set to %lf", TABLE_HEIGHT);
+    }
+    else{
+        ROS_INFO("Default: Height of the table is set to %lf", TABLE_HEIGHT);
+    }
+    
+    if(node_handle.getParam("object_height", OBJECT_HEIGHT)){
+        ROS_INFO("Height of the object is set to %lf", OBJECT_HEIGHT);
+    }
+    else{
+        ROS_INFO("Default: Height of the object is set to %lf", OBJECT_HEIGHT);
+    }
+    
+    if(node_handle.getParam("object_width", OBJECT_WIDTH)){
+        ROS_INFO("Width of the object is set to %lf", OBJECT_WIDTH);
+    }
+    else{
+        ROS_INFO("Default: Width of the object is set to %lf", OBJECT_WIDTH);
+    }
+    
+    if(node_handle.getParam("eef_offset", EEF_OFFSET)){
+        ROS_INFO("End effector offset value is set to %lf", EEF_OFFSET);
+    }
+    else{
+        ROS_INFO("Default: End effector offset value is set to %lf", EEF_OFFSET);
+    }
+    
+    if(node_handle.getParam("extra_offset", EXTRA_OFFSET)){
+        ROS_INFO("Extra offset value is set to %lf", EXTRA_OFFSET);
+    }
+    else{
+        ROS_INFO("Default: Extra offset value is set to %lf", EXTRA_OFFSET);
+    }   
+}
 
 void openGripper(trajectory_msgs::JointTrajectory& posture){
-    ROS_INFO("Check 1");
     posture.joint_names.resize(2);
     posture.joint_names[0] = "hand_finger_1";
     posture.joint_names[1] = "hand_finger_2";
 
-    ROS_INFO("Check 2");
     posture.points.resize(1);
     posture.points[0].positions.resize(2);
     posture.points[0].positions[0] = -0.03;
@@ -47,11 +87,11 @@ void pick(moveit::planning_interface::MoveGroupInterface& move_group){
 
     grasps[0].grasp_pose.header.frame_id = "base_link";
     tf2::Quaternion orientation;
-    orientation.setRPY(-M_PI / 2, -M_PI / 4, -M_PI / 2);
+    orientation.setRPY(-M_PI / 2, 0, -M_PI / 2);
     grasps[0].grasp_pose.pose.orientation = tf2::toMsg(orientation);
-    grasps[0].grasp_pose.pose.position.x = ALL_DIST - EEF_OFFSET - EXTRA_OFFSET;
+    grasps[0].grasp_pose.pose.position.x = TABLE_DIST - EEF_OFFSET - EXTRA_OFFSET;
     grasps[0].grasp_pose.pose.position.y = 0;
-    grasps[0].grasp_pose.pose.position.z = OBJECT_Z;
+    grasps[0].grasp_pose.pose.position.z = TABLE_HEIGHT + OBJECT_HEIGHT / 2;
 
     grasps[0].pre_grasp_approach.direction.header.frame_id = "base_link";
     grasps[0].pre_grasp_approach.direction.vector.x = 1.0;
@@ -63,19 +103,15 @@ void pick(moveit::planning_interface::MoveGroupInterface& move_group){
     grasps[0].post_grasp_retreat.min_distance = 0.05;
     grasps[0].post_grasp_retreat.desired_distance = 0.07;
 
-    ROS_INFO("Open gripper");
     openGripper(grasps[0].pre_grasp_posture);
     ros::WallDuration(1.0).sleep();
 
-    ROS_INFO("Close gripper");
     closedGripper(grasps[0].grasp_posture);
     ros::WallDuration(1.0).sleep();
 
-    ROS_INFO("Set support surface name");
     move_group.setSupportSurfaceName("table1");
     ros::WallDuration(1.0).sleep();
 
-    ROS_INFO("Pick object");
     move_group.pick("object", grasps);
     ros::WallDuration(1.0).sleep();
 }
@@ -86,11 +122,11 @@ void place(moveit::planning_interface::MoveGroupInterface& group){
 
     place_location[0].place_pose.header.frame_id = "base_link";
     tf2::Quaternion orientation;
-    orientation.setRPY(0, 0, M_PI / 2);
+    orientation.setRPY(-M_PI / 2, 0, 0);
     place_location[0].place_pose.pose.orientation = tf2::toMsg(orientation);
     place_location[0].place_pose.pose.position.x = 0;
-    place_location[0].place_pose.pose.position.y = ALL_DIST - EEF_OFFSET - EXTRA_OFFSET;
-    place_location[0].place_pose.pose.position.z = OBJECT_Z;
+    place_location[0].place_pose.pose.position.y = TABLE_DIST - EEF_OFFSET - EXTRA_OFFSET;
+    place_location[0].place_pose.pose.position.z = TABLE_HEIGHT + OBJECT_HEIGHT / 2;
 
     place_location[0].pre_place_approach.direction.header.frame_id = "base_link";
     place_location[0].pre_place_approach.direction.vector.z = -1.0;
@@ -125,7 +161,7 @@ void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface& pla
 
     collision_objects[0].primitive_poses.resize(1);
     collision_objects[0].primitive_poses[0].orientation.w = 1.0;
-    collision_objects[0].primitive_poses[0].position.x = ALL_DIST;
+    collision_objects[0].primitive_poses[0].position.x = TABLE_DIST;
     collision_objects[0].primitive_poses[0].position.y = 0;
     collision_objects[0].primitive_poses[0].position.z = TABLE_HEIGHT / 2;
 
@@ -144,7 +180,7 @@ void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface& pla
     collision_objects[1].primitive_poses.resize(1);
     collision_objects[1].primitive_poses[0].orientation.w = 1.0;
     collision_objects[1].primitive_poses[0].position.x = 0;
-    collision_objects[1].primitive_poses[0].position.y = ALL_DIST;
+    collision_objects[1].primitive_poses[0].position.y = TABLE_DIST;
     collision_objects[1].primitive_poses[0].position.z = TABLE_HEIGHT / 2;
 
     collision_objects[1].operation = collision_objects[1].ADD;
@@ -157,13 +193,13 @@ void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface& pla
     collision_objects[2].primitives[0].dimensions.resize(3);
     collision_objects[2].primitives[0].dimensions[0] = OBJECT_WIDTH;
     collision_objects[2].primitives[0].dimensions[1] = OBJECT_WIDTH;
-    collision_objects[2].primitives[0].dimensions[2] = OBJECT_LENGTH;
+    collision_objects[2].primitives[0].dimensions[2] = OBJECT_HEIGHT;
 
     collision_objects[2].primitive_poses.resize(1);
     collision_objects[2].primitive_poses[0].orientation.w = 1.0;
-    collision_objects[2].primitive_poses[0].position.x = ALL_DIST;
+    collision_objects[2].primitive_poses[0].position.x = TABLE_DIST;
     collision_objects[2].primitive_poses[0].position.y = 0;
-    collision_objects[2].primitive_poses[0].position.z = TABLE_HEIGHT + OBJECT_LENGTH / 2;
+    collision_objects[2].primitive_poses[0].position.z = TABLE_HEIGHT + OBJECT_HEIGHT / 2;
 
     collision_objects[2].operation = collision_objects[2].ADD;
 
@@ -172,7 +208,7 @@ void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface& pla
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "pick_and_place");
-    ros::NodeHandle node_handle;
+    ros::NodeHandle node_handle("~");
     ros::AsyncSpinner spinner(1);
     spinner.start();
 
@@ -180,6 +216,9 @@ int main(int argc, char** argv){
     moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
     moveit::planning_interface::MoveGroupInterface group("penguin_arm");
     group.setPlanningTime(45.0);
+
+    setParameters(node_handle);
+    ros::WallDuration(1.0).sleep();
 
     addCollisionObjects(planning_scene_interface);
 
